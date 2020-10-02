db575debcbfa81970fcc30ba13d44fef4fc23abb
simo54 <sorzesimone1@gmail.com>
2020-10-02 14:30:20 +0200

    trainlist and stoplist (to be merged as general overview) implemented, general fixes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Maintenance from "./Components/Maintenance";
+import Maintenance from "../src/Components/Maintenance";
 import Mover from "./Components/Mover";
+import ListofTrains from "./Components/ListofTrains";
+import ListofStops from "./Components/ListofStops";
 
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <nav>
           <ul>
             <li>
-              <Link className="link" to="/">
-                Home
+              <Link className='link' to='/'>
+                Trains
               </Link>
             </li>
             <li>
-              <Link className="link" to="/mover">
+              <Link className='link' to='/stops'>
+                Stops
+              </Link>
+            </li>
+            <li>
+              <Link className='link' to='/mover'>
                 Mover
               </Link>
             </li>
             <li>
-              <Link className="link" to="/maintenance">
+              <Link className='link' to='/maintenance'>
                 Maintenance
               </Link>
             </li>
@@ -30,11 +37,16 @@ const App = () => {
         </nav>
 
         <Switch>
-          <Route exact path="/"></Route>
-          <Route exact path="/mover">
+          <Route exact path='/'>
+            <ListofTrains />
+          </Route>
+          <Route exact path='/stops'>
+            <ListofStops />
+          </Route>
+          <Route exact path='/mover'>
             <Mover />
           </Route>
-          <Route exact path="/maintenance">
+          <Route exact path='/maintenance'>
             <Maintenance />
           </Route>
         </Switch>
