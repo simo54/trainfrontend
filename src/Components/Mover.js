b5f3b2035f8451b191248c0bce530d41ec707b3f
cravero.dvd@gmail.com <66950139+davidecravero@users.noreply.github.com>
2020-10-03 16:00:14 +0200

    Update Mover.js
    Added the functionality to fetch only the trains not in maintenance (RunningtrainsList).
    added basic html inside the jsx file.

--- src/Components/Mover.js
+++ src/Components/Mover.js
@@ -1,7 +1,46 @@
 import React from "react";
+import axios from "axios";
 
-const Mover = () => {
-  return <div>Test</div>;
-};
+export default function Mover() {
+  const [RunningtrainsList, setRunningTrainsList] = React.useState();
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:5000/trains/running")
+      .then(function (response) {
+        const arrayList = response.data;
+        setRunningTrainsList(arrayList);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-export default Mover;
+  return (
+    <div className="RunningTrainContainer">
+      <h2>Running Trains List</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>city</th>
+            <th>Length</th>
+            <th className="empty">Stop</th>
+          </tr>
+        </thead>
+        <tbody>
+          {RunningtrainsList
+            ? RunningtrainsList.map((element, index) => (
+                <tr key={index}>
+                  <td>{element.id}</td>
+                  <td>{element.name}</td>
+                  <td>{element.length}</td>
+                  <td className="empty">{element.stopid}</td>
+                </tr>
+              ))
+            : null}
+        </tbody>
+      </table>
+    </div>
+  );
+}
